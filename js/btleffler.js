ca40924468c6dd80e8a0ffdbbb0f105cd46bb169
Benjamin Leffler <btleffler@gmail.com>
2015-12-11 13:28:49 -0500

    Add node-hash-cli

--- js/btleffler.js
+++ js/btleffler.js
@@ -28,6 +28,7 @@ $(function() {
 	 '\t},\n' +
 	 '\t"Ajaxer": "<a href="http://btleffler.github.io/Ajaxer">http://btleffler.github.io/Ajaxer</a>",\n' +
 	 '\t"GitHub": "<a href="https://github.com/btleffler" title="btleffler on Github">https://github.com/btleffler</a>",\n' +
+	 '\t"hash-cli": "<a href="http://btleffler.github.io/node-hash-cli">http://btleffler.github.io/node-hash-cli</a>",\n' +
 	 '\t"jQuery-SwapStyle": "<a href="http://btleffler.github.io/jQuery-SwapStyle" title="jQuery-SwapStyle">http://btleffler.github.io/jQuery-SwapStyle</a>",\n' +
 	 '\t"Lunarized-Solo": "<a href="https://atom.io/themes/lunarized-solo-syntax" title="Lunarized-Solo">https://atom.io/themes/lunarized-solo-syntax</a>,\n' +
 	 '\t"Node File-List": "<a href="https://www.npmjs.org/package/file-list" title="Node File-List">https://www.npmjs.org/package/file-list</a>,\n' +
@@ -147,6 +148,7 @@ $(function() {
 		},
 		"Ajaxer": "http://btleffler.github.io/Ajaxer",
 		"GitHub": "https://github.com/btleffler",
+		"hash-cli": "http://btleffler.github.io./node-hash-cli",
 		"jQuery-SwapStyle": "http://btleffler.github.io/jQuery-SwapStyle",
 		"Lunarized-Solo": "https://atom.io/themes/lunarized-solo-syntax",
 		"Node File-List": "https://www.npmjs.org/package/file-list",
