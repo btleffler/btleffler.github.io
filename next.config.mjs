30e9febd8eeb8b496254c85bbb79f8de2e5b993b
btleffler <btleffler@gmail.com>
2024-11-23 11:03:16 -0500

    Attempt fix build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
   /**
    * Waiting for Nextjs Sass implementation to be updated
