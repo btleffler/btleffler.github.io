6d3355313450f8e5d461e48ab523c560066e3935
btleffler <btleffler@gmail.com>
2025-02-03 09:39:36 -0500

    Remove sass silencing

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  /**
-   * Waiting for Nextjs Sass implementation to be updated
-   * https://github.com/vercel/next.js/issues/71638#issuecomment-2431137842
-   */
-  sassOptions: {
-    silenceDeprecations: ['legacy-js-api'],
-  },
 };
 
 export default nextConfig;
