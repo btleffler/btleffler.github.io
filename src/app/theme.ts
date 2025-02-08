62aff6d1d315b0ddb2c2cf04acebda9648ca741f
btleffler <btleffler@gmail.com>
2025-02-08 11:17:26 -0500

    Responsive font sizes

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,31 +1,33 @@
 'use client';
 
-import { createTheme } from '@mui/material/styles';
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
-export const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#3fa3b5',
+export const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#3fa3b5',
+      },
+      secondary: {
+        main: '#85d1ff',
+      },
+      error: {
+        main: '#f30000',
+      },
+      warning: {
+        main: '#fffc00',
+      },
+      info: {
+        main: '#216ef3',
+      },
+      success: {
+        main: '#24cb00',
+      },
+      divider: 'rgba(255,255,255,0.13)',
     },
-    secondary: {
-      main: '#85d1ff',
+    shape: {
+      borderRadius: 3,
     },
-    error: {
-      main: '#f30000',
-    },
-    warning: {
-      main: '#fffc00',
-    },
-    info: {
-      main: '#216ef3',
-    },
-    success: {
-      main: '#24cb00',
-    },
-    divider: 'rgba(255,255,255,0.13)',
-  },
-  shape: {
-    borderRadius: 3,
-  },
-});
+  })
+);
